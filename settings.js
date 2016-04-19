534cffdde15ad49f1badedefb9eb3fcf726df8ed
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-19 14:05:24 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,24 @@
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "3000",
+    "hideDuration": "1000",
+    "timeOut": "3500",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+System = {
+    error: function(message) {
+        toastr.error(message)
+        throw new Error(message);
+    }
+}
