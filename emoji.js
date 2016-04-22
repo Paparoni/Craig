ba0c6a4b5587aeed615e7fd350debfb817c2cae6
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-22 08:30:20 -0400

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,3 +1,14 @@
-if(Peanut.contains(reply, ":)") == true){
-    reply = reply.replace(/:)/g, "<img src=\"https://assets-cdn.github.com/images/icons/emoji/bowtie.png\">");
+var emoji = function(text, src) {
+        this.text = text;
+        this.src = src;
+    }
+    // Create emojis
+var emojis = [smile];
+var smile = new emoji(":)", "https://assets-cdn.github.com/images/icons/emoji/bowtie.png")
+for (var e = 0; e < emojis.length; e++) {
+    var ec = function(c_reply) {
+        if (Peanut.contains(c_reply, emojis[e].text) == true) {
+            reply = reply.replace(/emojis[e].text/g, "<img src=\"" + emojis[e].src + "\">");
+        }
+    }
 }
