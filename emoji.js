9c94992113fa4660546201cd2e51c5719f63026d
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-22 09:04:30 -0400

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -7,14 +7,12 @@ function e_filter(text) {
   }, url = "https://assets-cdn.github.com/images/icons/emoji/unicode/", patterns = [],
      metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
-  // build a regex pattern for each defined property
   for (var i in emoticons) {
-    if (emoticons.hasOwnProperty(i)){ // escape metacharacters
+    if (emoticons.hasOwnProperty(i)){ 
       patterns.push('('+i.replace(metachars, "\\$&")+')');
     }
   }
 
-  // build the regular expression and replace
   return text.replace(new RegExp(patterns.join('|'),'g'), function (match) {
     return typeof emoticons[match] != 'undefined' ?
            '<img src="'+url+emoticons[match]+'" height=\"20\" width=\"20\"/>' :
