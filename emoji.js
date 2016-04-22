9fdf72b59a02e46cad0db56faec513f1732ec427
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-22 09:04:11 -0400

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,14 +1,23 @@
-var emoji = function(text, src) {
-        this.text = text;
-        this.src = src;
+function e_filter(text) {
+  var emoticons = {
+    ':-)' : '1f60a.png',
+    ':)'  : '1f60a.png',
+    ':D'  : '1f604.png',
+    ':('  : '1f61f.png'
+  }, url = "https://assets-cdn.github.com/images/icons/emoji/unicode/", patterns = [],
+     metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
+
+  // build a regex pattern for each defined property
+  for (var i in emoticons) {
+    if (emoticons.hasOwnProperty(i)){ // escape metacharacters
+      patterns.push('('+i.replace(metachars, "\\$&")+')');
     }
-    // Create emojis
-var emojis = [smile];
-var smile = new emoji(":)", "https://assets-cdn.github.com/images/icons/emoji/bowtie.png")
-for (var e = 0; e < emojis.length; e++) {
-    var ec = function(c_reply) {
-        if (Peanut.contains(c_reply, emojis[e].text) == true) {
-            reply = reply.replace(/emojis[e].text/g, "<img src=\"" + emojis[e].src + "\">");
-        }
-    }
+  }
+
+  // build the regular expression and replace
+  return text.replace(new RegExp(patterns.join('|'),'g'), function (match) {
+    return typeof emoticons[match] != 'undefined' ?
+           '<img src="'+url+emoticons[match]+'" height=\"20\" width=\"20\"/>' :
+           match;
+  });
 }
