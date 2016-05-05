79c82797f00ca99ec75f0ca78c6ca0f238f8f837
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 13:51:12 -0400

    Update log.js

--- depend/log.js
+++ depend/log.js
@@ -3,10 +3,18 @@
 Save chat logs and download them as txt
 
 */
+
 var log_array = [];
 function addTolog(bot_message, user_message){
-  
+  this.bot_message = bot_message;
+  this.user_message = user_message;
+  log_array.push("("+Peanut.time()+") Craig: "+bot_message+"\r\n");
+  log_array.push("("+Peanut.time()+") You: "+user_message+"\r\n");
 }
 function saveLog(){
-  
+        var blob_save = log_array.toString().split(',').join(' ');
+        var blob = new Blob([blob_save], {
+            type: "text/plain;charset=utf-8"
+        });
+        saveAs(blob, System.date.month+"-"+System.date.day+"-"+System.date.year+"log.txt");
 }
