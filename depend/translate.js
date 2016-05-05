bfa8a37c6ad4c12454f2b2e8a5735b7c18dae002
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 09:27:05 -0400

    Update translate.js

--- depend/translate.js
+++ depend/translate.js
@@ -1,20 +1,22 @@
-// translate.js
-function translate(language, input) {
-    this.input = input
-    this.language = language
-    if (language.toLowerCase() == "spanish") {
-        language = "es";
-        Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
-            return JSON.parse(response).text
-        });
-    } else if (language.toLowerCase() == "french") {
-        language = "fr";
-        Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
-            return JSON.parse(response).text
-        });
-    } else {
-        return "Invalid Language";
-    }
+// translates craig's replies
 
+function translate_fr(input) {
+    this.input = input
+    var language = "fr";
+    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
+        var voidof = JSON.parse(response).text
+        var translated = voidof.toString();
+        $('#outputCS').append('<b><font color=\'green\'>Craig:</b></font>' + e_filter(translated) + '<br/>');
+    });
+}
+
+function translate_es(input) {
+    this.input = input
+    var language = "es";
+    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
+        var voidof = JSON.parse(response).text
+        var translated = voidof.toString();
+        $('#outputCS').append('<b><font color=\'green\'>Craig:</b></font>' + e_filter(translated) + '<br/>');
+    });
 
 }
