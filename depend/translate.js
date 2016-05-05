ba2c9bde15fe1e266036d152dedc7e36fb5571c5
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 13:59:27 -0400

    timestamps

--- depend/translate.js
+++ depend/translate.js
@@ -6,7 +6,7 @@ function translate_fr(input) {
     Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
         var voidof = JSON.parse(response).text
         var translated = voidof.toString();
-        $('#outputCS').append('<b><font color=\'green\'>Craig:</b></font>' + e_filter(translated) + '<br/>');
+        $('#outputCS').append('<b><font color=\'green\'>('+Peanut.time()+') Craig:</b></font>' + e_filter(translated) + '<br/>');
     });
 }
 
@@ -16,7 +16,7 @@ function translate_es(input) {
     Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
         var voidof = JSON.parse(response).text
         var translated = voidof.toString();
-        $('#outputCS').append('<b><font color=\'green\'>Craig:</b></font>' + e_filter(translated) + '<br/>');
+        $('#outputCS').append('<b><font color=\'green\'>('+Peanut.time()+') Craig:</b></font>' + e_filter(translated) + '<br/>');
     });
 
 }
