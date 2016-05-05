66069698a7112393a71a0b1701392f4af518aa94
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 13:46:47 -0400

    date settings

--- depend/settings.js
+++ depend/settings.js
@@ -1,3 +1,23 @@
+/*/ System Variables /*/
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var m = 
+var day = d.getDay();
+var year = d.getYear();
+/*/ End of system variables /*/
+
 toastr.options = {
     "closeButton": false,
     "debug": false,
@@ -20,5 +40,12 @@ System = {
     error: function(message) {
         toastr.error(message)
         throw new Error(message);
+    },
+    
+    date: {
+        month: month[d.getMonth()],
+        day: d.getDay(),
+        year: d.getYear()
     }
+    
 }
