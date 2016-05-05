e694c7faf5d988a229f9441dcf6a9e83c58f0428
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 08:41:46 -0400

    Update translate.js

--- depend/translate.js
+++ depend/translate.js
@@ -1,24 +1,20 @@
 // translate.js
-function translate(language, input){
-  this.input = input
-  this.language = language
-  if(language.toLowerCase() == "spanish"){
-    language = "es";
-    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&text=" + encodeURIComponent(language), function(response) {
-      var x = JSON.parse(response);
-      return x.text;
-      
-    })
-  } else if(language.toLowerCase() == "french"){
-    language = "fr";
-    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&text=" + encodeURIComponent(language), function(response) {
-      var x = JSON.parse(response);
-      return x.text;
-      
-    });
-  } else {
-    return "Invalid Language";
-  }
-  
-  
+function translate(language, input) {
+    this.input = input
+    this.language = language
+    if (language.toLowerCase() == "spanish") {
+        language = "es";
+        Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
+            return JSON.parse(response).text
+        });
+    } else if (language.toLowerCase() == "french") {
+        language = "fr";
+        Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&lang=en-" + encodeURIComponent(language) + "&text=" + encodeURIComponent(input), function(response) {
+            return JSON.parse(response).text
+        });
+    } else {
+        return "Invalid Language";
+    }
+
+
 }
