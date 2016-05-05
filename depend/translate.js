520490b2e19166f1a8f5859225d0930a0865df44
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 08:37:55 -0400

    Update translate.js

--- depend/translate.js
+++ depend/translate.js
@@ -1,8 +1,24 @@
 // translate.js
-function translate(language){
+function translate(language, input){
+  this.input = input
   this.language = language
   if(language.toLowerCase() == "spanish"){
-    language = "es"
+    language = "es";
+    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&text=" + encodeURIComponent(language), function(response) {
+      var x = JSON.parse(response);
+      return x.text;
+      
+    })
+  } else if(language.toLowerCase() == "french"){
+    language = "fr";
+    Peanut.loadJSON("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20160503T130721Z.febfceae36b8944c.5671ad1d6b9648cadc9815344695c9448a29c665&text=" + encodeURIComponent(language), function(response) {
+      var x = JSON.parse(response);
+      return x.text;
+      
+    });
+  } else {
+    return "Invalid Language";
   }
+  
   
 }
